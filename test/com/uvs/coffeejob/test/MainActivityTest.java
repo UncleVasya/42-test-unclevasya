d1d3873f236d57d393f7d1e0ec021697257d00dd
UncleVasya (Oleg Ovcharenko) <Oleg_Ovcharenko@list.ru>
2013-10-10 22:01:39 +0300

    ticket:1. A bunch of new tests. Ready to write code.

--- test/com/uvs/coffeejob/test/MainActivityTest.java
+++ test/com/uvs/coffeejob/test/MainActivityTest.java
@@ -15,14 +15,20 @@ import com.uvs.coffeejob.User;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
-	final String DEFAULT_NAME    = "Oleg";
-	final String DEFAULT_SURNAME = "Ovcharenko";
-	final String DEFAULT_BIO     = "Nothing interesting here. " + 
-                                   "Just a man from Belogorsk.";
+	final String DEFAULT_NAME     = "Oleg";
+	final String DEFAULT_SURNAME  = "Ovcharenko";
+	final String DEFAULT_BIO      = "Nothing interesting here. " + 
+                                    "Just a man from Belogorsk.";
+	final String DEFAULT_BIRTH    = "January 01, 1991";
+	final String DEFAULT_CONTACTS = "ICQ: 1234567" + "\n" +
+                                    "Jabber: uvs.jabber.org" + "\n" +
+                                    "Phone: 9379992";
  
 	private MainActivity mMainActivity;
 	private TextView mUserName;
 	private TextView mUserBio;
+	private TextView mUserBirth;
+	private TextView mUserContacts;
 	
 	@Before
 	public void setUp() {
@@ -30,12 +36,16 @@ public class MainActivityTest {
 		mMainActivity.onCreate(null);
 		mUserName = (TextView)mMainActivity.findViewById(R.id.userNameText);
 		mUserBio = (TextView)mMainActivity.findViewById(R.id.userBioText);
+		mUserBirth = (TextView)mMainActivity.findViewById(R.id.userBirthText);
+		mUserContacts = (TextView)mMainActivity.findViewById(R.id.userContactsText);
 	}
 	
 	@Test 
 	public void test_UserShown() {
 		assertEquals(DEFAULT_NAME + " " + DEFAULT_SURNAME, mUserName.getText().toString());
 		assertEquals(DEFAULT_BIO, mUserBio.getText().toString());
+		assertEquals(DEFAULT_BIRTH, mUserBirth.getText().toString());
+		assertEquals(DEFAULT_CONTACTS, mUserContacts.getText().toString());
 	}
 }
 
--- test/com/uvs/coffeejob/test/UserTest.java
+++ test/com/uvs/coffeejob/test/UserTest.java
@@ -2,11 +2,14 @@ package com.uvs.coffeejob.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.GregorianCalendar;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 import com.uvs.coffeejob.User;
+import com.uvs.coffeejob.UserContact;
 
 @RunWith(RobolectricTestRunner.class)
 public class UserTest {
@@ -14,6 +17,7 @@ public class UserTest {
 	final String DEFAULT_SURNAME = "Ovcharenko";
 	final String DEFAULT_BIO     = "Nothing interesting here. " + 
                                    "Just a man from Belogorsk.";
+	final GregorianCalendar DEFAULT_BIRTH = new GregorianCalendar(1991, 01, 01);
     
 	@Test
     public void test_UserSetters() {
@@ -21,9 +25,58 @@ public class UserTest {
         user.setName(DEFAULT_NAME);
         user.setSurname(DEFAULT_SURNAME);
         user.setBio(DEFAULT_BIO);
+        user.setBirthDate(DEFAULT_BIRTH);
         assertEquals(DEFAULT_NAME, user.getName());
         assertEquals(DEFAULT_SURNAME, user.getSurname());
         assertEquals(DEFAULT_BIO, user.getBio());
+        assertEquals(DEFAULT_BIRTH, user.getBirthDate());
+    }
+	
+	@Test
+    public void test_UserAddContact() {
+		final String CONTACT_1_TYPE    = "ICQ";
+		final String CONTACT_1_VALUE   = "1234567";
+		
+		final String CONTACT_2_TYPE    = "Jabber";
+		final String CONTACT_2_VALUE   = "uvs.jabber.org";
+		
+        User user = new User();
+        assertEquals(0, user.getContacts().size());
+        
+        user.addContact(CONTACT_1_TYPE, CONTACT_1_VALUE);
+        assertEquals(1, user.getContacts().size());
+        
+        UserContact contact = new UserContact(CONTACT_2_TYPE, CONTACT_2_VALUE);
+        user.addContact(contact);
+        assertEquals(2, user.getContacts().size());
+        
+        assertEquals(new UserContact(CONTACT_1_TYPE, CONTACT_1_VALUE),  
+                     user.getContacts().get(0));
+        assertEquals(new UserContact(CONTACT_2_TYPE, CONTACT_2_VALUE),  
+                user.getContacts().get(1));
+    }
+	
+	@Test
+    public void test_UserClearContacts() {
+		final String CONTACT_1_TYPE    = "ICQ";
+		final String CONTACT_1_VALUE   = "1234567";
+		
+		final String CONTACT_2_TYPE    = "Jabber";
+		final String CONTACT_2_VALUE   = "uvs.jabber.org";
+		
+        User user = new User();
+        
+        user.addContact(CONTACT_1_TYPE, CONTACT_1_VALUE);
+        user.addContact(CONTACT_2_TYPE, CONTACT_2_VALUE);
+        assertEquals(2, user.getContacts().size());
+        
+        user.clearContacts();
+        assertEquals(0, user.getContacts().size());
+        
+        user.addContact(CONTACT_1_TYPE, CONTACT_1_VALUE);
+        assertEquals(1, user.getContacts().size());
+        
+        user.clearContacts();
+        assertEquals(0, user.getContacts().size());
     }
 }
-
