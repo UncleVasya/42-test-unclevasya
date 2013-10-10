c13989d291e32b675b03e1b534a5f917278cedbc
UncleVasya (Oleg Ovcharenko) <Oleg_Ovcharenko@list.ru>
2013-10-10 17:12:17 +0300

    ticket:1. Cut away all working code. Tests should go first.

--- src/com/uvs/coffeejob/MainActivity.java
+++ src/com/uvs/coffeejob/MainActivity.java
@@ -21,27 +21,15 @@ public class MainActivity extends Activity {
 	}
 	
 	private User setupDefaultUser() {
-		final String DEFAULT_NAME    = "Oleg";
-		final String DEFAULT_SURNAME = "Ovcharenko";
-		final String DEFAULT_BIO     = "Nothing interesting here. " +
-							           "Just a human from Belogorsk";
 		User user = new User();
-		user.setName(DEFAULT_NAME);
-		user.setSurname(DEFAULT_SURNAME);
-		user.setBio(DEFAULT_BIO);
 		return user;
 	}
 
 	private void showUserInfo(User user) {
-		clearUserInfo();
-		if (user != null) {
-			mUserName.setText(user.getName() + " " + user.getSurname());
-			mUserBio.setText(user.getBio());
-		}
+		
 	}
 	
 	private void clearUserInfo() {
-		mUserName.setText(null);
-		mUserBio.setText(null);
+
 	}
 }
