e11c2b8e69198fb72970557744c3e34b80280469
UncleVasya (Oleg Ovcharenko) <Oleg_Ovcharenko@list.ru>
2013-10-11 16:29:23 +0300

    ticket:1 . Updated test for MainActivity.

--- test/com/uvs/coffeejob/test/MainActivityTest.java
+++ test/com/uvs/coffeejob/test/MainActivityTest.java
@@ -1,6 +1,11 @@
 package com.uvs.coffeejob.test;
 
 import static org.junit.Assert.assertEquals;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +16,9 @@ import android.widget.TextView;
 
 import com.uvs.coffeejob.MainActivity;
 import com.uvs.coffeejob.R;
+import com.uvs.coffeejob.User;
+import com.uvs.coffeejob.UserContact;
+import com.uvs.coffeejob.UserManager;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
@@ -22,6 +30,8 @@ public class MainActivityTest {
 	final String DEFAULT_CONTACTS = "ICQ: 1234567" + "\n" +
                                     "Jabber: uvs.jabber.org" + "\n" +
                                     "Phone: 9379992";
+	
+	private UserManager mUserManager;
  
 	private MainActivity mMainActivity;
 	private TextView mUserName;
@@ -37,13 +47,28 @@ public class MainActivityTest {
 		mUserBio = (TextView)mMainActivity.findViewById(R.id.userBioText);
 		mUserBirth = (TextView)mMainActivity.findViewById(R.id.userBirthText);
 		mUserContacts = (TextView)mMainActivity.findViewById(R.id.userContactsText);
+		mUserManager = UserManager.getInstance();
 	}
 	
 	@Test 
 	public void test_UserShown() {
-		assertEquals(DEFAULT_NAME + " " + DEFAULT_SURNAME, mUserName.getText().toString());
-		assertEquals(DEFAULT_BIO, mUserBio.getText().toString());
-		assertEquals(DEFAULT_BIRTH, mUserBirth.getText().toString());
-		assertEquals(DEFAULT_CONTACTS, mUserContacts.getText().toString());
+	    User user = mUserManager.getUser();
+		assertEquals(user.getName() + " " + user.getSurname(), mUserName.getText().toString());
+		assertEquals(user.getBio(), mUserBio.getText().toString());
+		
+		SimpleDateFormat format = new SimpleDateFormat("MMMM dd, yyyy", Locale.ENGLISH);
+		assertEquals(format.format(user.getBirthDate().getTime()), mUserBirth.getText().toString());
+		
+		StringBuilder str_contacts = new StringBuilder();
+        if (user.getContacts() != null) {
+            List<UserContact> contacts = user.getContacts();
+            UserContact contact = contacts.get(0);
+            str_contacts.append(contact.getType() + ": " + contact.getValue());
+            for (int i=1; i<user.getContacts().size(); ++i) {
+                contact = contacts.get(i);
+                str_contacts.append("\n" + contact.getType() + ": " + contact.getValue());
+            }
+        }
+		assertEquals(str_contacts.toString(), mUserContacts.getText().toString());
 	}
 }
